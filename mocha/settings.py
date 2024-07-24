6947ecc46b00327f8f5f2010c92af33b315660e3
xenomech <gokulsmenon227@gmail.com>
2024-07-24 08:25:46 +0530

    chore: Update build script to include pip install and gunicorn command

--- mocha/settings.py
+++ mocha/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "mocha.urls"
@@ -128,6 +129,14 @@ STATIC_URL = "static/"
 STATIC_FILES_DIRS = [
     os.path.join(BASE_DIR, "todo/static"),
 ]
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
