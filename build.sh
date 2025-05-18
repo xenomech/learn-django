af29ca62c02b4ca0c285c3663d028aa0921b67d7
xenomech <gokulsmenon227@gmail.com>
2025-05-19 01:40:22 +0530

    Revert "Revert "deployment config""
    This reverts commit 54998bad0161b4444c629c4317c9486d4410b571.

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
 
-# gunicorn mocha.wsgi:application --bind
+gunicorn mocha.wsgi:application --bind 0.0.0.0:8000 --workers 3
