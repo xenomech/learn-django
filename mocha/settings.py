5c80f7184dc402cfb7ad875e30a1e2028b1d5930
xenomech <gokulsmenon227@gmail.com>
2025-05-19 01:39:20 +0530

    deployment config

--- mocha/settings.py
+++ mocha/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "mocha.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default="postgresql://xenomech@localhost:5432/mocha",
+        default="postgresql://xenomech:xenomech@db:5432/mocha",
         conn_max_age=600,
     )
 }
